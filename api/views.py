cd066d086ee3c7b7e5fc008f134ca3ddb2fcfd4a
AnshikaDixit <dixitanshika09@gmail.com>
2024-09-19 12:41:59 +0530

    ADDED --> api to create student

--- api/views.py
+++ api/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render
 from .models import Student
 from .serializers import StudentSerializer
 from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
 from django.http import HttpResponse, JsonResponse
+import io
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
 # Model bject - Single Student Data
@@ -18,4 +21,21 @@ def student_list(request):
     stu = Student.objects.all()
     serializer = StudentSerializer(stu, many=True)
     json_data = JSONRenderer().render(serializer.data)
+    return HttpResponse(json_data, content_type='application/json')
+
+
+@csrf_exempt
+def student_create(request):
+    if request.method == "POST":
+        json_data = request.body
+        stream = io.BytesIO(json_data)
+        pythondata = JSONParser().parse(stream)
+        serializer = StudentSerializer(data=pythondata)
+        if serializer.is_valid():
+           serializer.save()
+           res = {'msg' : "Data Craeted"}
+           json_data = JSONRenderer().render(res)
+           return HttpResponse(json_data, content_type='application/json')
+    
+    json_data = JSONRenderer().render(serializer.errors)
     return HttpResponse(json_data, content_type='application/json')
